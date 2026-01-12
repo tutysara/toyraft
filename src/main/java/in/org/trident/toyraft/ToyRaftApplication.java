813efe8204ada13a873f5b8e1a4eddbf6c5246c2
Saran Baskaran <tutysara@gmail.com>
2026-01-12 22:39:22 +0530

    inital commit

---
+++ src/main/java/in/org/trident/toyraft/ToyRaftApplication.java
@@ -0,0 +1,13 @@
+package in.org.trident.toyraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyRaftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/org/trident/toyraft/ToyRaftApplicationTests.java
@@ -0,0 +1,13 @@
+package in.org.trident.toyraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyRaftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
